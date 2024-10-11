c2dbfef03ab102db524dfa6a9c56f6400bf2b81c
saviola0191 <oyedejisaviola@gmail.com>
2024-10-11 22:04:33 +0100

    commit

--- BetterHealth/src/Components/SignIn.jsx
+++ BetterHealth/src/Components/SignIn.jsx
@@ -4,15 +4,11 @@ import {Link} from "react-router-dom"
 const SignIn = ({username, password}) => {
   return (
     <>
-    <div className='w-[28vw] lg:w-[10vw] p-2 bg-blue-600 mt-[20px] lg:mt-[-2px] text-center rounded-xl'>
+    <div className='w-[28vw] lg:w-[10vw] p-2 bg-[#0073cf] mt-[20px] lg:mt-[-2px] text-center rounded-xl'>
    <Link className='text-white ' to={`/`}>Sign in</Link>
 
     </div>
-   
-
-
-    {/* <button className=' mt-5 w-28 h-10 bg-violet-600 text-gray-400 rounded-lg text-[13px] font-semibold'onClick={()=>{alert(``)
-    }}>Sign in</button> */}
+  
     </>
   )
 }
