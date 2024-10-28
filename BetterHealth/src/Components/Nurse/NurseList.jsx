072e2e51fdb443df13e6d1acaffa185bc2412cc5
solomonojox <solomonakpas@gmail.com>
2024-10-28 22:37:13 +0100

    akpas

--- BetterHealth/src/Components/Nurse/NurseList.jsx
+++ BetterHealth/src/Components/Nurse/NurseList.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import SideBar from '../SideBar';
 import Navbar from '../Navbar';
-import { nurseList } from './NurseData';
+// import { nurseList } from './NurseData';
 import { GoHome } from "react-icons/go";
+import axios from 'axios';
 
 const NurseList = () => {
+    const navigate = useNavigate()
+    const [nurses, setNurses] = useState([]);
+    useEffect(() => {
+        const fetchNurse = async () => {
+            try{
+                const res = await axios.get('https://hms-w4kw.onrender.com/api/Nurse/GetAllNurses')
+                setNurses(res.data.data.$values)
+                // console.log(res.data.data.$values)
+            } catch(error){
+                console.log(error.response.data)
+            }
+        }
+        fetchNurse()
+    }, [])
   return (
    <div className="flex flex-col gap-0 lg:flex-row py-4 md:px-0 ">
         <SideBar/>
@@ -29,19 +44,18 @@ const NurseList = () => {
 
                 <div className='py-5 md:ml-72 lg:ml-0 gap-x-10 grid grid-cols-1 lg:grid-cols-3 gap-y-10 '>
 
-                    {nurseList.map(nurse => {
+                    {nurses.map(nurse => {
                         return(
-                           <NavLink  to={"/nursedetails"} >
-                             <div key={nurse.id} className='w-[250px] overflow-hidden shadow-2xl border border-stone-100 rounded-lg bg-white'>
+                            <div key={nurse.nurseId} className='w-[250px] overflow-hidden shadow-2xl border border-stone-100 rounded-lg bg-white'
+                                onClick={() => navigate('/nursedetails', {state: nurse.nurseId})}>
                                 <div className='flex justify-center flex-col items-center h-[200px] w-[100%] '>
                                     <img src={nurse.thumb} alt="image" className='w-100% h-[100%] object-cover ' />
                                 </div>
                                 <div className='text-2xl text-white font-medium bg-primary2 flex flex-col items-center p-2 '>
-                                    <p className=''>{nurse.name}</p>
-                                    <p className='font-medium text-sm text-gray-300'>{nurse.title}</p>
+                                    <p className=''>{nurse.firstname} {nurse.lastname}</p>
+                                    <p className='font-medium text-sm text-gray-300'>{nurse.specialization}</p>
                                 </div>
                             </div>
-                           </NavLink>
                         )
                     })}
                 </div>
